fd8f49ebe5ff73e419117ecea3647727243a73b2
cancer21072002 <92455145+cancer21072002@users.noreply.github.com>
2022-10-03 11:09:34 +0700

    use v-form

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -1,19 +1,30 @@
 <template>
     <div class="menu">
         <ul class="flex">
-            <li><a href="" class="bold text-uppercase hover-blue">Chăm sóc da</a></li>
-            <li><a href="" class="bold text-uppercase hover-blue">Trang điểm</a></li>
-            <li><a href="" class="bold text-uppercase hover-blue">Thời trang</a></li>
-            <li><a href="" class="bold text-uppercase hover-blue">Lifestyle</a></li>
-            <li><a href="" class="bold text-uppercase hover-blue">Kiểu tóc</a></li>
-            <li><a href="" class="bold text-uppercase hover-blue">Nail</a></li>
-            <li><a href="" class="bold text-uppercase hover-blue">Mẹo hay</a></li>
+            <li v-for="(link, index) in links" :key="index">
+                <a href="" class="bold text-uppercase hover-blue">{{ link }}</a>
+            </li>
         </ul>
     </div>
 </template>
 
-<script setup>
-
+<script>
+export default {
+    data () {
+        return {
+            links: [
+                
+                "Chăm sóc da",
+                "Trang điểm",
+                "Thời trang",
+                "Lifestyle",
+                "Kiểu tóc",
+                "Nail",
+                "Mẹo hay",
+            ]
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/content/TopImage.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="top-images flex">
+        <div class="left-image center-background cursor-pointer">
+            <div class="top-labels">
+                <p class="bold white-text text-center">04/01/2022</p>
+                <h2 class="bold white-text text-center block">Bật mí 7 bí quyết chăm sóc tóc bồng bềnh, óng ả dành cho
+                    các cô nàng tóc dầu</h2>
+            </div>
+        </div>
+        <div class="right-images">
+            <div class="upper-image center-background cursor-pointer">
+                <div class="top-labels">
+                    <p class="bold white-text text-center">31/12/2021</p>
+                    <h2 class="bold white-text text-center">Lấy lại tự tin với 18+ kiểu trang phục cho cô nàng vai rộng
+                        siêu nữ tính</h2>
+                </div>
+            </div>
+            <div class="down-images flex">
+                <div class="down-image center-background cursor-pointer"
+                    style="background-image: url(https://xinhxinh.vn/wp-content/uploads/2021/12/trang-phuc-mau-hong-dang-yeu.png);">
+                    <div class="top-labels">
+                        <p class="bold white-text text-center">31/12/2021</p>
+                        <h2 class="bold white-text text-center">Ngọt ngào với 18+ trang phục màu hồng đáng yêu cực kì nữ
+                            tính</h2>
+                    </div>
+                </div>
+                <div class="down-image center-background cursor-pointer"
+                    style="background-image: url(https://xinhxinh.vn/wp-content/uploads/2021/12/image-3.jpeg);">
+                    <div class="top-labels">
+                        <p class="bold white-text text-center">25/12/2021</p>
+                        <h2 class="bold white-text text-center">15 cách chăm sóc tóc mềm mượt, chắc khoẻ cực đơn giản
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+
+</style>
